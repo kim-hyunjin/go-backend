541387c7e8393f4e104e2727156f5ef4c82d46b3
Kim <hyunjin1612@gmail.com>
2021-04-03 14:17:14 +0900

    테스트 코드 만들기

--- main.go
+++ main.go
@@ -1,55 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
+
+	"github.com/kim-hyunjin/go-web/myapp"
 )
 
-type User struct {
-	FirstName string	`json:"first_name"`
-	LastName string		`json:"last_name"`
-	Email string		`json:"email"`
-	CreatedAt time.Time	`json:"created_at"`
-}
-
-type fooHandler struct{}
-
-func (f *fooHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
-	if err != nil {
-		rw.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(rw, "Bad Request: ", err)
-		return
-	}
-	user.CreatedAt = time.Now()
-
-	data, _ := json.Marshal(user)
-	rw.Header().Add("content-type", "application/json")
-	rw.WriteHeader(http.StatusOK)
-	fmt.Fprint(rw, string(data))
-}
-
-func barHandler (rw http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "world"
-	}
-	fmt.Fprintf(rw, "heelo, %s!", name)
-}
 
 func main() {
-	mux := http.NewServeMux()
-	fmt.Println("Server Started!")
-	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(rw, "hello world")
-	})
-
-	mux.HandleFunc("/bar", barHandler)
-
-	mux.Handle("/foo", &fooHandler{})
-
-	http.ListenAndServe(":3000", mux)
+	http.ListenAndServe(":3000", myapp.NewHttpHandler())
 }
--- note.md
+++ note.md
@@ -8,3 +8,14 @@ type User struct {
 ```
 
 구조체에 `json:"first_name"` 이런식으로 태그를 달아두면 구조체의 변수와 json의 key가 달라서 발생하는 문제를 해결할 수 있다.
+
+## 테스트 컨벤션
+
+- 파일명은 끝에 \_test를 붙인다.
+- 메소드명은 Test로 시작한다.
+- 아규먼트로 \*testing.T 를 받는다.
+
+### 원활한 테스트를 위한 라이브러리
+
+- github.com/smartystreets/goconvey : 파일이 변경될때마다 자동을 테스트를 수행해준다.
+- github.com/stretchr/testify : assert 패키지의 메소드를 사용하면 쉽게 테스트할 수 있다.
