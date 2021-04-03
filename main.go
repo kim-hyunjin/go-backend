cde073ea2f3b4b0f394b99c6fbaf386dd2061ec3
Kim <hyunjin1612@gmail.com>
2021-04-03 12:59:27 +0900

    http handler

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 	"net/http"
 )
 
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello Foo!")
+}
+
 func main() {
+	fmt.Println("Server Started!")
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(rw, "hello world")
 	})
+
+	http.HandleFunc("/bar", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "heelo Bar")
+	})
+
+	http.Handle("/foo", &fooHandler{})
 
 	http.ListenAndServe(":3000", nil)
 }
