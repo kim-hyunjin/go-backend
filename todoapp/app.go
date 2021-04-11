bdd1abe0fd58793947e882ebb8a04298a60889cd
Kim <hyunjin1612@gmail.com>
2021-04-11 20:38:26 +0900

    test code, refactoring

--- todoapp/app.go
+++ todoapp/app.go
@@ -3,71 +3,51 @@ package main
 import (
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/kim-hyunjin/go-web/todoapp/model"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
 )
 
-var rd *render.Render
-
-type Todo struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Completed bool `json:"completed"`
-	CreatedAt time.Time `json:"created_at"`
+type Success struct {
+	Success bool `json:"success"`
 }
 
-var todoMap map[int]*Todo
+var rd *render.Render
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/todo.html", http.StatusTemporaryRedirect)
 }
 
 func getTodosHandler(w http.ResponseWriter, r *http.Request) {
-	list := []*Todo{}
-	for _, v := range todoMap {
-		list = append(list, v)
-	}
+	list := model.GetTodos()
 	rd.JSON(w, http.StatusOK, list)
-}
-
-func addTestTodos() {
-	todoMap[1] = &Todo{1, "Buy a milk", false, time.Now()}
-	todoMap[2] = &Todo{2, "Exercise", true, time.Now()}
-	todoMap[3] = &Todo{3, "Home work", true, time.Now()}
 }
 
 func addTodoHandler(w http.ResponseWriter, r *http.Request) {
 	value := r.FormValue("name")
-	id := len(todoMap) + 1
-	todoMap[id] = &Todo{id, value, false, time.Now()}
-	rd.JSON(w, http.StatusOK, todoMap[id])
-}
-
-type Success struct {
-	Success bool `json:"success"`
+	todo := model.AddTodo(value)
+	rd.JSON(w, http.StatusCreated, todo)
 }
 
 func deleteTodoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
-	if _, ok := todoMap[id]; ok {
-		delete(todoMap, id)
+	ok := model.DeleteTodo(id)
+	if ok {
 		rd.JSON(w, http.StatusOK, Success{true})
 	} else {
 		rd.JSON(w, http.StatusOK, Success{false})
-	}
-	
+	}	
 }
 
 func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 	complete := r.FormValue("complete") == "true"
-	if todo, ok := todoMap[id]; ok {
-		todo.Completed = complete
+	ok := model.CompleteTodo(id, complete)
+	if ok {
 		rd.JSON(w, http.StatusOK, Success{true})
 	} else {
 		rd.JSON(w, http.StatusOK, Success{false})
@@ -75,8 +55,6 @@ func completeTodoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func MakeHandler() http.Handler {
-	todoMap = make(map[int]*Todo)
-	addTestTodos()
 	rd = render.New()
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", indexHandler)
